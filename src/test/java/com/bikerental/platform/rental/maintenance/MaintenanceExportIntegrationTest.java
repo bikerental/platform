e2cd90a86cee121c66f3c16d502959b6eeb5f590
baduke3520 <luch0002@stud.kea.dk>
2025-12-30 23:15:40 +0100

    refactor: move admin auth to database

--- src/test/java/com/bikerental/platform/rental/maintenance/MaintenanceExportIntegrationTest.java
+++ src/test/java/com/bikerental/platform/rental/maintenance/MaintenanceExportIntegrationTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,12 +31,6 @@ import com.bikerental.platform.rental.bike.repo.BikeRepository;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@TestPropertySource(properties = {
-        "jwt.secret=test-secret-key-that-is-at-least-256-bits-long-for-hmac-sha-256-algorithm",
-        "jwt.expiration-hours=10",
-        "admin.username=admin",
-        "admin.password-hash=$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
-})
 @SuppressWarnings({"null", "unused"})
 class MaintenanceExportIntegrationTest {
 
